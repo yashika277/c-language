1ba7a26182600ff3579ad7ca496fc5b753ad265f
yashika277 <116864909+yashika277@users.noreply.github.com>
2023-06-06 18:06:16 +0530

    Add files via upload

---
+++ print/1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	printf("Name: Bhadkoliya yashika\n");
+	printf("Age: 19 years\n");
+	printf("collage: SDJ international collage,vesu");
+	return 0;
+}
---
+++ print/a_z.c
@@ -0,0 +1,236 @@
+#include<stdio.h>
+int main(){
+//	A
+	printf("* * * * *\n");
+	printf("*       *\n");
+	printf("* * * * *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n\n\n");
+	
+//	B
+	printf("* * *\n");
+	printf("*     *\n");
+	printf("*    *\n");
+	printf("* * *\n");
+	printf("*    *\n");
+	printf("*     *\n");
+	printf("* * *\n\n\n");
+	
+//	C
+	printf("* * * *\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("* * * *\n\n\n");
+	
+//	D
+	printf("* *\n");
+	printf("*   *\n");
+	printf("*    *\n");
+	printf("*    *\n");
+	printf("*    *\n");
+	printf("*   *\n");
+	printf("* *\n\n\n");
+	
+//	E
+	printf("* * * *\n");
+	printf("*\n");
+	printf("*\n");
+	printf("* * * *\n");
+	printf("*\n");
+	printf("*\n");
+	printf("* * * *\n\n\n");
+		
+//	F
+	printf("* * * *\n");
+	printf("*\n");
+	printf("*\n");
+	printf("* * * *\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n\n\n");
+	
+//	G
+	printf(" * * *\n");
+	printf("*      *\n");
+	printf("*\n");
+	printf("*   * * *\n");
+	printf("*       *\n");
+	printf("*      *\n");
+	printf(" * * *\n\n\n");
+	
+//	H
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("* * * *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n\n\n");
+	
+//	I
+	printf("* * * * *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("* * * * *\n\n\n");
+	
+//	J
+	printf(" * * * * *\n");
+	printf("     *\n");
+	printf("     *\n");
+	printf("     *\n");
+	printf("     *\n");
+	printf("*    *\n");
+	printf(" *  *\n\n\n");
+	
+//	K
+	printf("*   *\n");
+	printf("*  *\n");
+	printf("* *\n");
+	printf("**\n");
+	printf("* *\n");
+	printf("*  *\n");
+	printf("*   *\n\n\n");
+	
+//	L
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("* * * *\n\n\n");
+	
+//	M
+	printf("*      *\n");
+	printf("* *  * *\n");
+	printf("*   *  *\n");
+	printf("*      *\n");
+	printf("*      *\n");
+	printf("*      *\n");
+	printf("*      *\n\n\n");
+	
+//	N
+	printf("*    *\n");
+	printf("*    *\n");
+	printf("* *  *\n");
+	printf("*  * *\n");
+	printf("*   **\n");
+	printf("*    *\n");
+	printf("*    *\n\n\n");
+	
+//	o
+	printf(" * * *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");    
+	printf("*     *\n");
+	printf(" * * *\n\n\n");
+	
+//	P
+	printf("* * * *\n");
+	printf("*      *\n");
+	printf("*      *\n");
+	printf("* * * *\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n\n\n");
+	
+//	Q
+	printf(" * * *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*  *  *\n");
+	printf("*     *\n");
+	printf(" *  * *\n");
+	printf("        *\n\n\n");
+	
+//	R
+	printf("* * *\n");
+	printf("*    *\n");
+	printf("*    *\n");
+	printf("* * *\n");
+	printf("*    *\n");
+	printf("*     *\n");
+	printf("*      *\n\n\n");
+	
+//	S
+	printf(" * * * *\n");
+	printf("*\n");
+	printf("*\n");
+	printf(" * * *\n");
+	printf("       *\n");
+	printf("       *\n");
+	printf(" * * * *\n\n\n");
+	
+// 	T
+	printf("* * * * *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("    *\n");
+	printf("    *\n\n\n");	
+	
+//	U
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf(" * * *\n\n\n");
+	
+//	V
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf(" *     *\n");
+	printf("    *\n\n\n");
+	
+//	W
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*   *   *\n");
+	printf("* *   * *\n");
+	printf("*       *\n\n\n");
+	
+//	X
+	printf("*       *\n");
+	printf(" *     *\n");
+	printf("  *   *\n");
+	printf("    *\n");
+	printf("  *   *\n");
+	printf(" *     *\n");
+	printf("*       *\n\n\n");
+	
+//	Y
+	printf("*     *\n");
+	printf("*     *\n");
+	printf(" *   *\n");
+	printf("   *\n");
+	printf("   *\n");
+	printf("   *\n");
+	printf("   *\n\n\n");
+	
+//	z
+	printf("* * * * *\n");
+	printf("       *\n");
+	printf("      *\n");
+	printf("    *\n");
+	printf("   *\n");
+	printf(" *\n");
+	printf("* * * * *\n\n");
+	return 0;
+}
